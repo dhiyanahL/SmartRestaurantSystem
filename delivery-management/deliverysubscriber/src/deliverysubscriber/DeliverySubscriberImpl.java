67923cf9d65c35897d27f61e087f5c9b23603e3c
Dhanushi Piyaratne <123639982+dhanushi01@users.noreply.github.com>
2025-03-11 19:11:31 +0530

    Update DeliverySubscriberImpl.java

--- delivery-management/deliverysubscriber/src/deliverysubscriber/DeliverySubscriberImpl.java
+++ delivery-management/deliverysubscriber/src/deliverysubscriber/DeliverySubscriberImpl.java
@@ -1,21 +1,13 @@
 package deliverysubscriber;
 
+import deliveryapi.DeliverySubscriber; // Import from deliveryapi
 import ordermanagementosgi.DeliveryOrder;
 
 public class DeliverySubscriberImpl implements DeliverySubscriber {
-
     @Override
     public void onDeliveryCompleted(DeliveryOrder deliveryOrder) {
-        try {
-            System.out.println("📦 Delivery Completed!");
-            System.out.println("Order ID: " + deliveryOrder.getDeliveryOrderId());
-            System.out.println("Item: " + deliveryOrder.getType());
-            System.out.println("Quantity: " + deliveryOrder.getQuantity());
-            System.out.println("Customer: " + deliveryOrder.getCustomerName());
-            System.out.println("Phone: " + deliveryOrder.getPhone());
-            System.out.println("Address: " + deliveryOrder.getAddress());
-        } catch (Exception e) {
-            System.err.println("Error processing delivery completion: " + e.getMessage());
-        }
+    	 System.out.println("Delivery completed for order: " + deliveryOrder.getDeliveryOrderId());
+         System.out.println("Customer Name: " + deliveryOrder.getCustomerName());
+         System.out.println("Order Details: " + deliveryOrder.toString());
     }
 }
