5ecc42808b7fe14eaf9efe871a7cbdd1abd01388
SachiniGayathma <sachinigayathma@yahoo.com>
2025-03-18 00:02:56 +0530

    Order Management As An Independant Unit "

---
+++ orderManagement/OrderManagementProducer/src/ordermanagementproducer/Activator.java
@@ -0,0 +1,38 @@
+package ordermanagementproducer;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    private static BundleContext context;
+    private OrderService orderService;
+
+    static BundleContext getContext() {
+        return context;
+    }
+
+    public void start(BundleContext bundleContext) throws Exception {
+        System.out.println("Hi, OSGi Bundle Started!");
+        context = bundleContext;
+
+        // Create an instance of the OrderServiceImpl
+        orderService = new OrderServiceImpl();
+        
+        // Register the OrderService with the OSGi framework
+        bundleContext.registerService(OrderService.class.getName(), orderService, null);
+        
+        System.out.println("OrderService registered.");
+    }
+
+    public void stop(BundleContext bundleContext) throws Exception {
+        System.out.println("Hello, OSGi Bundle Stopped!");
+
+        // Unregister the OrderService
+        if (orderService != null) {
+            bundleContext.ungetService(bundleContext.getServiceReference(OrderService.class.getName()));
+        }
+
+        System.out.println("OrderService unregistered.");
+    }
+}
---
+++ orderManagement/OrderManagementProducer/src/ordermanagementproducer/DineInOrder.java
@@ -0,0 +1,98 @@
+package ordermanagementproducer;
+
+public class DineInOrder {
+	
+
+	private int dineInOrderId;
+	private String type;
+	private int quantity;
+	private String customer;
+	private String phone;
+	private int tableNo;
+	
+	
+	public DineInOrder(int dineInOrderId, String type, int quantity, String customer, String phone, int tableNo) {
+		super();
+		this.dineInOrderId = dineInOrderId;
+		this.type = type;
+		this.quantity = quantity;
+		this.customer = customer;
+		this.phone = phone;
+		this.tableNo = tableNo;
+	}
+
+
+	public int getDineInOrderId() {
+		return dineInOrderId;
+	}
+
+
+	public void setDineInOrderId(int dineInOrderId) {
+		this.dineInOrderId = dineInOrderId;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+
+	public String getCustomer() {
+		return customer;
+	}
+
+
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+	public int getTableNo() {
+		return tableNo;
+	}
+
+
+	public void setTableNo(int tableNo) {
+		this.tableNo = tableNo;
+	}
+	
+	
+    public String toString() {
+        return "Order Details: " +
+               "\nID: " + dineInOrderId +
+               "\nItem Name: " + type +
+               "\nQuantity: " + quantity +
+               "\nCustomer Name: " + customer +
+               "\nContact No: " + phone +
+               "\nTable No: " + tableNo;
+    }
+	
+	
+
+
+}
